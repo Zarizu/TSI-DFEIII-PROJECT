2052daf2bf5ea0534e758213cc086369fc0dad91
Caputi <lcaputi2@gmail.com>
2025-11-02 16:50:02 -0300

    refactor: add utils/views folder

--- utils/views/main-views-utils.js
+++ utils/views/main-views-utils.js
@@ -1,46 +1,4 @@
 
-
-//logica e sistemas
-
-function checkBattleReady() {
-    const actionsSet = Object.keys(playerActions).length;
-    const teamSize = team.length;
-    
-    // Só permite começar se o time tiver membros
-    if (teamSize > 0 && actionsSet === teamSize) {
-        startBattleButton.disabled = false;
-        startBattleButton.textContent = "Começar Batalha!";
-    } else {
-        startBattleButton.disabled = true;
-        startBattleButton.textContent = `Selecione Ações (${actionsSet}/${teamSize})`;
-    }
-}
-
-function executeRound() {
-    if (startBattleButton.disabled) return;
-
-    console.log("Batalha Iniciada! Ações:", playerActions);
-    
-    // --- LÓGICA DO JOGO (FUTURO) ---
-    //chamaria a lógica de processar as 'playerActions'
-    //chamaria a IA para as ações inimigas
-    //atualizaria o 'team' (HP, mana, efeitos)
-    //chamaria a 'refreshAllUI()' para redesenhar tudo
-    //
-
-    // sistema de round
-    roundNumberSpan.textContent = GAME_MANAGER.passRound();
-    
-    // Limpa as ações para o próximo round
-    playerActions = {};
-    playerArea.querySelectorAll('.action-icon').forEach(icon => {
-        icon.classList.remove('selected');
-    });
-
-    // Desabilita o botão novamente
-    checkBattleReady();
-}
-
 //draws (desenha os estados atuais na tela)
 function refreshAllUI() {
     team.forEach(character => {
